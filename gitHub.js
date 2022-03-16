8a10da3316d78ea2877a7c86747a1489f3f86992
Joe Medina <joemedina97@aol.com>
2022-03-16 13:33:27 -0500

    Adding header to github constructor 3/16/22 1:32pm

--- gitHub.js
+++ gitHub.js
@@ -3,18 +3,22 @@ class GitHub{
   constructor(){
   this.client_id = 'cb0a380006cf04590522'
   this.client_secret ='206e12c282568d57e478ad60aa66c8158f1e408b'
-  this.Access_token = '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'
+  this.Access_token = {
+  Headers:{
+    Authentication:'token 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'
   }
+
+  }}
   //this get the users
    async getusers(user){
   
   const profileResponse = await fetch(`https://api.github.com/users/${user}
   ?client_id=${user.client_id}&client_secret=${user.client_secret}`)
   const getProfileData = await profileResponse.json()
-
+  this.Access_token
     return{
       getProfileData
     }
   
-  
+
 }}
